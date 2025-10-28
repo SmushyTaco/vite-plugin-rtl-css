891b7ad98a9182be08aa3117069d1ecebc0d8c61
Nikan Radan <github.com@alias.nikanradan.com>
2025-10-28 13:43:21 -0700

    Dependency Updates

--- README.md
+++ README.md
@@ -15,9 +15,8 @@ import { defineConfig } from 'vite';
 import viteRtlCssPlugin from 'vite-plugin-rtl-css';
 
 export default defineConfig({
-    plugins: [viteRtlCssPlugin()]
+  plugins: [viteRtlCssPlugin()]
 });
-
 ```
 
 This will transform css files to their rtl counterparts. Look below for options
@@ -26,17 +25,17 @@ This will transform css files to their rtl counterparts. Look below for options
 
 ```ts
 viteRtlCssPlugin({
-    fileNameMap: {
-        '.css': '[name].rtl.css',
-        '.min.css': '[name].rtl.min.css'
-    },
-    sourceMap: true,
-    rtlcssConfig: {
-        autoRename: true
-    }
+  fileNameMap: {
+    '.css': '[name].rtl.css',
+    '.min.css': '[name].rtl.min.css'
+  },
+  sourceMap: true,
+  rtlcssConfig: {
+    autoRename: true
+  }
 });
 ```
 
-* `fileNameMap` is an optional parameter that specifies a map of file extensions to templates for rtl css generation. What's seen in the example is the default.
-* `sourceMap` is an optional parameter that specifies if sourcemaps should be generated. If this isn't specified it'll default to `build.sourcemap` from your `vite.config.ts`.
-* `rtlcssConfig` is an optional parameter that comes from the `rtlcss` package that's used by this plugin to configure `rtlcss`.
+- `fileNameMap` is an optional parameter that specifies a map of file extensions to templates for rtl css generation. What's seen in the example is the default.
+- `sourceMap` is an optional parameter that specifies if sourcemaps should be generated. If this isn't specified it'll default to `build.sourcemap` from your `vite.config.ts`.
+- `rtlcssConfig` is an optional parameter that comes from the `rtlcss` package that's used by this plugin to configure `rtlcss`.
--- src/index.ts
+++ src/index.ts
@@ -3,90 +3,90 @@ import postcss from 'postcss';
 import rtlcss, { ConfigOptions } from 'rtlcss';
 
 type RtlCssPluginOptions = {
-    fileNameMap?: Record<string, string>;
-    sourceMap?: boolean | 'inline' | 'hidden';
-    rtlcssConfig?: ConfigOptions;
+  fileNameMap?: Record<string, string>;
+  sourceMap?: boolean | 'inline' | 'hidden';
+  rtlcssConfig?: ConfigOptions;
 };
 
 function viteRtlCssPlugin(options: RtlCssPluginOptions = {}): Plugin {
-    let sourcemapOption: boolean | 'inline' | 'hidden' = false;
-    const fileNameMap = options.fileNameMap || {
-        '.css': '[name].rtl.css',
-        '.min.css': '[name].rtl.min.css'
-    };
+  let sourcemapOption: boolean | 'inline' | 'hidden' = false;
+  const fileNameMap = options.fileNameMap || {
+    '.css': '[name].rtl.css',
+    '.min.css': '[name].rtl.min.css'
+  };
 
-    const getOutputFileName = (
-        inputFileName: string,
-        extension: string
-    ): string => {
-        const baseName = inputFileName.slice(0, -extension.length);
-        const template =
-            fileNameMap[extension] || `${baseName}.rtl${extension}`;
-        return template.replace('[name]', baseName);
-    };
+  const getOutputFileName = (
+    inputFileName: string,
+    extension: string
+  ): string => {
+    const baseName = inputFileName.slice(0, -extension.length);
+    const template = fileNameMap[extension] || `${baseName}.rtl${extension}`;
+    return template.replace('[name]', baseName);
+  };
 
-    // noinspection JSUnusedGlobalSymbols
-    return {
-        name: 'vite-rtlcss-plugin',
-        enforce: 'post',
+  // noinspection JSUnusedGlobalSymbols
+  return {
+    name: 'vite-rtlcss-plugin',
+    enforce: 'post',
 
-        configResolved(config) {
-            sourcemapOption = options.sourceMap ?? config.build.sourcemap;
-        },
+    configResolved(config) {
+      sourcemapOption = options.sourceMap ?? config.build.sourcemap;
+    },
 
-        async generateBundle(_, bundle) {
-            for (const [fileName, asset] of Object.entries(bundle)) {
-                if (asset.type !== 'asset') continue;
-                const sortedFileNameMapKeys = Object.keys(fileNameMap).sort(
-                    (a, b) => {
-                        const dotCountA = (a.match(/\./g) || []).length;
-                        const dotCountB = (b.match(/\./g) || []).length;
+    async generateBundle(_, bundle) {
+      for (const [fileName, asset] of Object.entries(bundle)) {
+        if (asset.type !== 'asset') continue;
+        const sortedFileNameMapKeys = Object.keys(fileNameMap).toSorted(
+          (a, b) => {
+            const dotCountA = (a.match(/\./g) || []).length;
+            const dotCountB = (b.match(/\./g) || []).length;
 
-                        // Sort by dot count (descending)
-                        if (dotCountA !== dotCountB) {
-                            return dotCountB - dotCountA;
-                        }
-
-                        // If dot counts are equal, sort by length (descending)
-                        return b.length - a.length;
-                    }
-                );
-
-                const extension = sortedFileNameMapKeys.find((key) =>
-                    fileName.endsWith(key)
-                );
-                if (!extension) continue;
-                const cssContent = asset.source?.toString();
-                if (!cssContent) continue;
-                const rtlFileName = getOutputFileName(fileName, extension);
-                const result = await postcss([
-                    rtlcss(options.rtlcssConfig)
-                ]).process(cssContent, {
-                    from: fileName,
-                    to: rtlFileName,
-                    map: {
-                        inline: sourcemapOption === 'inline',
-                        annotation: sourcemapOption !== 'hidden'
-                    }
-                });
-                this.emitFile({
-                    type: 'asset',
-                    fileName: rtlFileName,
-                    source: result.css
-                });
-                if (
-                    result.map &&
-                    (sourcemapOption === true || sourcemapOption === 'hidden')
-                ) {
-                    this.emitFile({
-                        type: 'asset',
-                        fileName: `${rtlFileName}.map`,
-                        source: result.map.toString()
-                    });
-                }
+            // Sort by dot count (descending)
+            if (dotCountA !== dotCountB) {
+              return dotCountB - dotCountA;
             }
+
+            // If dot counts are equal, sort by length (descending)
+            return b.length - a.length;
+          }
+        );
+
+        const extension = sortedFileNameMapKeys.find((key) =>
+          fileName.endsWith(key)
+        );
+        if (!extension) continue;
+        const cssContent = asset.source?.toString();
+        if (!cssContent) continue;
+        const rtlFileName = getOutputFileName(fileName, extension);
+        const result = await postcss([rtlcss(options.rtlcssConfig)]).process(
+          cssContent,
+          {
+            from: fileName,
+            to: rtlFileName,
+            map: {
+              inline: sourcemapOption === 'inline',
+              annotation: sourcemapOption !== 'hidden'
+            }
+          }
+        );
+        this.emitFile({
+          type: 'asset',
+          fileName: rtlFileName,
+          source: result.css
+        });
+        if (
+          result.map &&
+          (sourcemapOption === true || sourcemapOption === 'hidden')
+        ) {
+          this.emitFile({
+            type: 'asset',
+            fileName: `${rtlFileName}.map`,
+            source: result.map.toString()
+          });
         }
-    };
+      }
+    }
+  };
 }
 
 // noinspection JSUnusedGlobalSymbols
